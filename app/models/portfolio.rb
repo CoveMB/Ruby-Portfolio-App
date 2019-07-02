945834e5d1fa2172c8f0b767c42d71eeebec82c7
bjmrq <bmarquiscom@gmail.com>
2019-07-02 18:55:16 -0400

    Integrated technology model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,7 +1,12 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
+
+  has_many :technologies
   validates_presence_of :title, :subtitle, :body, :main_image, :thumb_image
 
   def self.data_engineering
     where(subtitle: "Data Engineering")
   end
+
+  after_initialize :set_defaults
 end
---
+++ db/migrate/20190702221913_addbadge_to_skills.rb
@@ -0,0 +1,4 @@
+class AddbadgeToSkills < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
