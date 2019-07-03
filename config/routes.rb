d0dcb0c66ce8f9b2dff617afebe1f2f35f6a78ee
bjmrq <bmarquiscom@gmail.com>
2019-07-03 10:28:39 -0400

    Implemented basic Devise configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   resources :portfolios, except: [:show, :edit]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'portfolio/:id/edit', to: 'portfolios#edit', as: 'portfolio_edit'
