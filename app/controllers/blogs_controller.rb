20a360e43d1c68e00ff4f3bf1c9b0976c854f302
bjmrq <bmarquiscom@gmail.com>
2019-07-03 17:50:16 -0400

    Refactoring all views

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,6 +5,7 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
+    byebug
     @blogs = Blog.all
   end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,11 @@ module ApplicationHelper
       (link_to "Login", new_user_session_path)
     end
   end
+
+  def source_helper(styles)
+    if session[:source]
+      greeting = "Thanks for visiting me from #{session[:source]}, please feel free to #{ link_to 'contact me', contact_path } if you'd like to work together."
+      content_tag(:div, greeting.html_safe, class: styles)
+    end
+  end
 end
