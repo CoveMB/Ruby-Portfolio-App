7b6b92b64831a2c18fbfdbf294695bd3b24c1c4f
bjmrq <bmarquiscom@gmail.com>
2019-07-03 12:00:48 -0400

    Add session source to Application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   include DeviseWhitelist
+
+  before_action :set_source
+
+  def set_source
+    session[:source] = params[:utm_source] if params[:utm_source]
+  end
 end
