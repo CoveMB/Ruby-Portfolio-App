0fd0c362266ee24098134ac8640eeb3c2bd2039a
bjmrq <bmarquiscom@gmail.com>
2019-07-04 14:29:24 -0400

    Updated style for user registration and login

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,13 @@
 module ApplicationHelper
   def login_helper style = ''
     if current_user.is_a?(GuestUser)
+      "<p class='nav-link'>&nbsp;|&nbsp;</p>".html_safe +
       (link_to "Register", new_user_registration_path, class: style) +
       " ".html_safe +
       (link_to "Login", new_user_session_path, class: style)
     else
-      link_to "Logout", destroy_user_session_path, method: :delete, class: style
+      "<p class='nav-link'>&nbsp;|&nbsp;</p>".html_safe +
+      (link_to "Logout", destroy_user_session_path, method: :delete, class: style)
     end
   end
 
